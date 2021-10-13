37c0ff763234b1c8828a103dd451f7ec181d027d
wanghuipeng <624793769@qq.com>
2021-10-13 15:12:28 +0800

    在node中请求第三方接口

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const koa = require('koa')
 const app = new koa()
 const path = require('path')
+const request = require('request');
 
 const views = require('koa-views');
 app.use(views(path.join(__dirname, './views'), { extension: 'ejs' }))
@@ -8,9 +9,13 @@ app.use(views(path.join(__dirname, './views'), { extension: 'ejs' }))
 app.use(async(ctx, next) => {
     console.log(ctx.request.path)
     if (ctx.request.path === '/index') { // 首页
-        // ctx.response.status = 200
-        // ctx.response.body = 'index'
-        await ctx.render('index')
+        let data = ''
+        await requestDemo().then((body) => {
+                data = JSON.parse(body)
+            })
+            // ctx.response.status = 200
+            // ctx.response.body = 'index'
+        await ctx.render('index', { data })
     } else if (ctx.request.path === '/hello') { // hello页
         ctx.response.status = 200
         ctx.response.body = 'hello world'
@@ -19,6 +24,19 @@ app.use(async(ctx, next) => {
     }
     await next()
 })
+async function requestDemo() {
+    let url = 'https://api.douban.com/v2/user/1000001?apikey=0df993c66c0c636e29ecbb5344252a4a'
+    return new Promise(function(resolve, reject) {
+        request(url, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                resolve(body);
+            } else {
+                reject('接口请求失败')
+            }
+
+        })
+    });
+}
 
 app.listen(3000, function() {
     console.log('koa应用启动！')
